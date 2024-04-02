43338faa139be6bd7849c678708d8088717b58d0
james <jamesparker0000@gmail.com>
2024-04-02 23:37:45 +0100

    fix

--- database/migrations/2024_04_02_060022_create_people_table.php
+++ database/migrations/2024_04_02_060022_create_people_table.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Models\company;
-use App\Models\company;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
